bf2ae1c98efa86d218c4f9d2ffc12b41d02db2ea
Alejandro Lopez <unity@Alejandros-iMac-8.local>
2019-01-26 20:53:43 -0500

    Google vr added

---
+++ Assets/VRScripts/VRKey.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class VRKey : MonoBehaviour
+{
+
+	public PlayerController _PlayerController;
+
+	public void ClickInKey(BaseEventData eventData)
+	{
+		gameObject.SetActive(false);
+	}
+}
