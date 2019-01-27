61c0dd989f0544aff2cf4d214a3d8d6d2096df47
Julián Valdés <julian@unity3d.com>
2019-01-27 12:49:28 -0500

    added particles for keys

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -5,8 +5,11 @@ using UnityEngine.EventSystems;
 
 public class Key : MonoBehaviour 
 {
+    [SerializeField]
+    private GameObject particle;
     public void ClickInKey(BaseEventData eventData)
     {
+        particle.SetActive(false);
         gameObject.SetActive(false);
     }
 }
