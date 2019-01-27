5d7e03507130254bc2facc9606908b403c554c17
Julián Valdés <julian@unity3d.com>
2019-01-27 15:18:20 -0500

    added new sound FXs

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour 
+{
+    public event System.Action onCompleted;
+    private Key[] maxKeys;
+    private List<Key> collectedKeys;
+
+	private void Start()
+	{
+        collectedKeys = new List<Key>();
+        maxKeys = FindObjectsOfType<Key>();
+	}
+
+    public void Collect (Key key)
+    {
+        if (!collectedKeys.Contains(key))
+            collectedKeys.Add(key);
+                     
+        if(collectedKeys.Count == maxKeys.Length)
+        {         
+			if (onCompleted != null)
+				onCompleted();
+        }
+    }
+}
---
+++ Assets/Scripts/RandomSoundAndPosition.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+public class RandomSoundAndPosition : RandomSound
+{
+    [SerializeField]
+    private float radius = 10;
+    protected override void PlayAction()
+    {
+        base.PlayAction();
+        transform.position = Random.insideUnitSphere * radius;
+    }
+}
