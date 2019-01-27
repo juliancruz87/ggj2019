31e18b6466234b40da6c86556436c21ae8cbfc9c
Alejandro Lopez <eniak23@gmail.com>
2019-01-27 15:08:06 -0500

    adding models

---
+++ Assets/Scripts/CharacterAttack.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class CharacterAttack : MonoBehaviour
+{
+	public  Animator anim;
+
+	private GameObject player;	
+	// Use this for initialization
+	void Start ()
+	{
+
+		var playerGO =  GameObject.FindObjectOfType<PlayerController>();
+		if (playerGO)
+			player = playerGO.gameObject;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+
+		if (player && Vector3.Distance(player.transform.position, transform.position) < 1)
+		{
+			anim.SetTrigger("Attack");
+		}
+
+	}
+}
